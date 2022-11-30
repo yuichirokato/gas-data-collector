d60aa29b70ab39e65d6f1e9ddf5e55d17ba52018
y.takahashi <ikmy0709@gmail.com>
2022-12-01 00:51:19 +0900

    feature: return all data

--- pages/api/get.ts
+++ pages/api/get.ts
@@ -1,9 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next"; 
 import { Redis } from '@upstash/redis/with-fetch';
+import { RedisKeys } from "../../utils/constants";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const redis = Redis.fromEnv();
     const key = "goerli-every-thirty-minutes";
     const data = await redis.lrange(key, 0, -1);
-    res.status(200).json(data);
+    const ethData = await redis.lrange(RedisKeys.ETHEREUM, 0, -1);
+    const goerliData = await redis.lrange(RedisKeys.GOERLI, 0, -1);
+    const polygonData = await redis.lrange(RedisKeys.POLYGON, 0, -1);
+
+    res.status(200).json({
+        ethereum: ethData,
+        goerli: goerliData,
+        polygon: polygonData
+    });
 }
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,11 @@
 const MAX_DATA_COUNT = 24 * 7
+const RedisKeys = {
+    GOERLI: 'goerli-every-thirty-minutes',
+    ETHEREUM: 'ethereum-every-thirty-minutes',
+    POLYGON: 'polygon-every-thirty-minutes'
+};
 
 export {
-    MAX_DATA_COUNT
+    MAX_DATA_COUNT,
+    RedisKeys
 }

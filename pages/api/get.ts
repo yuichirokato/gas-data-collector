4d55f633ca5fc754191b513c2ea7e731591205b5
y.takahashi <ikmy0709@gmail.com>
2022-12-24 20:45:35 +0900

    fix: 余分なデータ取得処理を削除

--- pages/api/get.ts
+++ pages/api/get.ts
@@ -4,8 +4,6 @@ import { RedisKeys } from "../../utils/constants";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const redis = Redis.fromEnv();
-    const key = "goerli-every-thirty-minutes";
-    const data = await redis.lrange(key, 0, -1);
     const ethData = await redis.lrange(RedisKeys.ETHEREUM, 0, -1);
     const goerliData = await redis.lrange(RedisKeys.GOERLI, 0, -1);
     const polygonData = await redis.lrange(RedisKeys.POLYGON, 0, -1);

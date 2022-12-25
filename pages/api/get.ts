7b17ecae63dd08e91dd6ba92d72fdd046cb869fb
y.takahashi <ikmy0709@gmail.com>
2022-12-25 21:47:18 +0900

    Fix: エラーログを吐かせるよう修正

--- pages/api/get.ts
+++ pages/api/get.ts
@@ -4,13 +4,19 @@ import { RedisKeys } from "../../utils/constants";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const redis = Redis.fromEnv();
-    const ethData = await redis.lrange(RedisKeys.ETHEREUM, 0, -1);
-    const goerliData = await redis.lrange(RedisKeys.GOERLI, 0, -1);
-    const polygonData = await redis.lrange(RedisKeys.POLYGON, 0, -1);
 
-    res.status(200).json({
-        ethereum: ethData,
-        goerli: goerliData,
-        polygon: polygonData
-    });
+    try {
+        const ethData = await redis.lrange(RedisKeys.ETHEREUM, 0, -1);
+        const goerliData = await redis.lrange(RedisKeys.GOERLI, 0, -1);
+        const polygonData = await redis.lrange(RedisKeys.POLYGON, 0, -1);
+
+        res.status(200).json({
+            ethereum: ethData,
+            goerli: goerliData,
+            polygon: polygonData
+        });
+    } catch (e) {
+        console.log("an error hs occured.", e);
+        res.status(500).send(`Unknown error occurred, reason: ${e}`);
+    }
 }
